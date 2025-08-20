9020325fee4312d9f5628f63ecb70586e985725a
Cristian Deleon <leoncristiand03@gmail.com>
2025-08-20 14:28:29 -0500

    Backend - scaffold Spring Boot project

---
+++ src/test/java/com/example/GymBackend/GymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
